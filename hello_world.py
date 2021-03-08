2c0ac4dd7efad737e315d91891709f589b557990
SrutiPraturi <39187327+SrutiPraturi@users.noreply.github.com>
2021-03-08 15:38:18 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,8 @@
-from flask import Flask
 
-app = Flask(__name__)
-@app.route('/')
-def index():
+def hello():
     
-    return 'Web App with Python Flask!'
+    return 'Hello World!'
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=81)
+  hello()
